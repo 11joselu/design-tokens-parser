33fd5545d07657fa5db203a45579fd5c5e437332
Jose Cabrera <jcabrera@habitissimo.com>
2020-08-10 19:12:02 +0200

    add less tests inside parseContent

--- test/parsers/parseContent.spec.ts
+++ test/parsers/parseContent.spec.ts
@@ -1,74 +1,149 @@
 import { parseContent, Node } from '../../src/parsers/parseContent';
 
 describe('parseContent', () => {
-  it('returns a Node instance', () => {
-    const parsedStyles = parseContent('', 'scss');
-    expect(parsedStyles).toMatchObject({
-      type: 'stylesheet',
-      content: [],
-      syntax: undefined,
-      start: [0, 0],
-      end: [0, 0],
+  describe('scss support', () => {
+    it('returns a Node instance', () => {
+      const parsedStyles = parseContent('', 'scss');
+      expect(parsedStyles).toMatchObject({
+        type: 'stylesheet',
+        content: [],
+        syntax: undefined,
+        start: [0, 0],
+        end: [0, 0],
+      });
     });
-  });
 
-  it("given 'scss' syntax parameter generate correct Node syntax attribute", () => {
-    const parsedStyles = parseContent('$color: red;', 'scss');
-
-    expect(parsedStyles.syntax).toBe('scss');
-  });
-
-  describe('Node length', () => {
-    it('generate a correct content length from inline styles', () => {
+    it("given 'scss' syntax parameter generate correct Node syntax attribute", () => {
       const parsedStyles = parseContent('$color: red;', 'scss');
 
-      expect(parsedStyles.content.length).toBe(2);
+      expect(parsedStyles.syntax).toBe('scss');
     });
 
-    it('generate a correct content length from multiple lines styles', () => {
-      const parsedStyles = parseContent(
-        `
+    describe('Node length', () => {
+      it('generate a correct content length from inline styles', () => {
+        const parsedStyles = parseContent('$color: red;', 'scss');
+
+        expect(parsedStyles.content.length).toBe(2);
+      });
+
+      it('generate a correct content length from multiple lines styles', () => {
+        const parsedStyles = parseContent(
+          `
       $color: red;
       `,
-        'scss'
-      );
+          'scss'
+        );
 
-      expect(parsedStyles.content.length).toBe(4);
-    });
+        expect(parsedStyles.content.length).toBe(4);
+      });
 
-    it('generate a correct content length from multiple lines styles', () => {
-      const parsedStyles = parseContent(
-        `
-      $color: red;
-      $border: none;
-      $fontSize: 16px;
-      `,
-        'scss'
-      );
-
-      const [spaceNode, declarationNode] = parsedStyles.content as Node[];
-
-      expect(spaceNode.type).toBe('space');
-      expect(declarationNode.type).toBe('declaration');
-      expect(parsedStyles.content.length).toBe(10);
-    });
-
-    it('generate a correct content value from multiple lines styles', () => {
-      const parsedStyles = parseContent(
-        `
+      it('generate a correct content length from multiple lines styles', () => {
+        const parsedStyles = parseContent(
+          `
       $color: red;
       $border: none;
       $fontSize: 16px;
       `,
-        'scss'
-      );
+          'scss'
+        );
 
-      const declarationNode = parsedStyles.first('declaration');
-      const valueNode = declarationNode.first('value');
-      const valueNodecontent = valueNode.content as Node[];
+        const [spaceNode, declarationNode] = parsedStyles.content as Node[];
 
-      expect(valueNode.is('value')).toBe(true);
-      expect(valueNodecontent[0].content).toBe('red');
+        expect(spaceNode.type).toBe('space');
+        expect(declarationNode.type).toBe('declaration');
+        expect(parsedStyles.content.length).toBe(10);
+      });
+
+      it('generate a correct content value from multiple lines styles', () => {
+        const parsedStyles = parseContent(
+          `
+      $color: red;
+      $border: none;
+      $fontSize: 16px;
+      `,
+          'scss'
+        );
+
+        const declarationNode = parsedStyles.first('declaration');
+        const valueNode = declarationNode.first('value');
+        const valueNodecontent = valueNode.content as Node[];
+
+        expect(valueNode.is('value')).toBe(true);
+        expect(valueNodecontent[0].content).toBe('red');
+      });
+    });
+  });
+
+  describe('less support', () => {
+    it('returns a Node instance', () => {
+      const parsedStyles = parseContent('', 'less');
+      expect(parsedStyles).toMatchObject({
+        type: 'stylesheet',
+        content: [],
+        syntax: undefined,
+        start: [0, 0],
+        end: [0, 0],
+      });
+    });
+
+    it("given 'less' syntax parameter generate correct Node syntax attribute", () => {
+      const parsedStyles = parseContent('@color: red;', 'less');
+
+      expect(parsedStyles.syntax).toBe('less');
+    });
+
+    describe('Node length', () => {
+      it('generate a correct content length from inline styles', () => {
+        const parsedStyles = parseContent('@color: red;', 'less');
+
+        expect(parsedStyles.content.length).toBe(2);
+      });
+
+      it('generate a correct content length from multiple lines styles', () => {
+        const parsedStyles = parseContent(
+          `
+          @color: red;
+      `,
+          'less'
+        );
+
+        expect(parsedStyles.content.length).toBe(4);
+      });
+
+      it('generate a correct content length from multiple lines styles', () => {
+        const parsedStyles = parseContent(
+          `
+            @color: red;
+            @border: none;
+            @fontSize: 16px;
+      `,
+          'less'
+        );
+
+        const [spaceNode, declarationNode] = parsedStyles.content as Node[];
+
+        expect(spaceNode.type).toBe('space');
+        expect(declarationNode.type).toBe('declaration');
+        expect(parsedStyles.content.length).toBe(10);
+      });
+
+      it('generate a correct content value from multiple lines styles', () => {
+        const parsedStyles = parseContent(
+          `
+            @color: red;
+            @border: none;
+            @fontSize: 16px;
+      `,
+          'less'
+        );
+
+        const declarationNode = parsedStyles.first('declaration');
+        const valueNode = declarationNode.first('value');
+        const valueNodecontent = valueNode.content as Node[];
+
+        expect(valueNode.is('value')).toBe(true);
+        expect(valueNodecontent[0].content).toBe('red');
+      });
     });
   });
 });
